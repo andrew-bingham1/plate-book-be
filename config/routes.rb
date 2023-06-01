433346ce38d7f4fdae4937f7e9c9c09a20c1627b
Andrew Bingham <andrewgbingham@gmail.com>
2023-06-01 17:12:00 -0600

    wip: create basic controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :posts
+      resources :plates
+    end
+  end
 end

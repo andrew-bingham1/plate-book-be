d55347d783f85904506ceead7e896900818440b1
Andrew Bingham <andrewgbingham@gmail.com>
2023-06-02 14:55:23 -0600

    feat: add show actions to controllers

--- app/controllers/api/v1/plates_controller.rb
+++ app/controllers/api/v1/plates_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::PlatesController < ApplicationController 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  
   def index
     render json: PlateSerializer.new(Plate.all)
+  end
+  
+  def show
+    render json: PlateSerializer.new(Plate.find(params[:id]))
+  end
+
+  private 
+
+  def record_not_found
+    render json: {"error":"Plate not found"}, status: :not_found
   end
 end

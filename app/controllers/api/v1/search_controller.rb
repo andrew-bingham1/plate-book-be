0452af600d71aa69ecb02e12c04490bb6cd3af71
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-06-03 15:41:40 -0600

    404 for nonexistent search category

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::SearchController < ApplicationController
   def index
     if params[:category] == "posts"
       render json: PostSerializer.new(Post.search(params[:query])), status: 200
+    elsif params[:categoty] == "plates"
+      render json: PostSerializer.new(Post.search(params[:query])), status: 200
+    else
+      render json: {error: "That category does not exist."}, status: 404
     end
   end
 end

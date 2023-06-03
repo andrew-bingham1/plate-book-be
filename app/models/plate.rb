52d95506e627dc7aaa8b0053c9bace818a4d9b85
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-06-03 16:15:58 -0600

    tests written for plate search

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -3,4 +3,9 @@ class Plate < ApplicationRecord
   has_many :posts, through: :plate_posts
 
   validates :plate_number, presence: true, uniqueness: true
+
+  def self.search(query)
+    query = query.gsub(" ", "").upcase
+    Plate.where("LOWER(plate_number) ILIKE ?", "%#{query}%")
+  end
 end
--- spec/models/plate_spec.rb
+++ spec/models/plate_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe Plate, type: :model do
     it { should have_many(:plate_posts) }
     it { should have_many(:posts).through(:plate_posts) }
   end
+
+  describe "class methods" do
+    before do 
+      @plate_1 = Plate.create!(plate_number: "EEE 111")
+      @plate_2 = Plate.create!(plate_number: "VEE 111")
+      @plate_3 = Plate.create!(plate_number: "VVE 111")
+      @plate_4 = Plate.create!(plate_number: "VVV 111")
+      @plate_5 = Plate.create!(plate_number: "VVV 112")
+      @plate_6 = Plate.create!(plate_number: "VVV 122")
+      @plate_7 = Plate.create!(plate_number: "VVV 222")
+      @plate_8 = Plate.create!(plate_number: "GRI 333")
+    end
+
+    it "partial match narrowing down" do
+      searches = ["1", "11", "111", "E111", "EE111", "EEE111"]
+      i = 6
+      searches.each do |query|
+        expect(Plate.search(query).length).to eq(i)
+        i -= 1
+      end
+    end
+  end
 end

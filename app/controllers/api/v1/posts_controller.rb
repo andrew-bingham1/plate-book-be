ca20f0fa9cdd6bd6ce63bd497bc6ede3296c89ba
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-06-03 13:44:20 -0600

    cleaner params for posts#create

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -6,12 +6,12 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def create
-    json = JSON.parse(params.to_json, symbolize_names: true)
+    new_params = JSON.parse(params[:params].to_json, symbolize_names: true)
     
-    if user = User.find_by_id(json[:params][:id])
-      post = Post.create(title: json[:params][:title], body: json[:params][:body], user_id: json[:params][:id])
-      plate = Plate.find_or_create_by(plate_number: json[:params][:plate_number])
-      plate.plate_posts.create(post_id: post.id, photo_url: json[:params][:photo_url])
+    if user = User.find_by_id(new_params[:id])
+      post = Post.create(title: new_params[:title], body: new_params[:body], user_id: new_params[:id])
+      plate = Plate.find_or_create_by(plate_number: new_params[:plate_number])
+      plate.plate_posts.create(post_id: post.id, photo_url: new_params[:photo_url])
       render json: PostSerializer.new(post), status: :created, status: 201
     else ActiveRecord::RecordNotFound
       render json: {errors: "User not found"}, status: 422

6b069cac252b4835a6602ac5ac4456ab96426b94
Andrew Bingham <andrewgbingham@gmail.com>
2023-06-04 15:07:36 -0600

    feat: update plate serializer to include posts

--- app/serializers/plate_serializer.rb
+++ app/serializers/plate_serializer.rb
@@ -4,4 +4,17 @@ class PlateSerializer
              :created_at,
              :updated_at
 
+  attribute :posts do |plate|
+    plate.posts.map do |post|
+      {
+        id: post.id,
+        title: post.title,
+        created_at: post.created_at.strftime("%I:%M %p %m/%d/%Y"),
+        username: post.user.username,
+        user_id: post.user_id,
+        comment_count: post.comments.count
+      }
+    end
+  end
+
 end

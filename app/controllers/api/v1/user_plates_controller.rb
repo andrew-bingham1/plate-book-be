d5022c88441dc0de826f53187362e4b45896997e
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-06-03 19:01:02 -0600

    sad paths for user_plate creation

--- app/controllers/api/v1/user_plates_controller.rb
+++ app/controllers/api/v1/user_plates_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::UserPlatesController < ApplicationController
     user_plate = UserPlate.new(user_id: params[:user_id], plate_id: params[:plate_id])
     if user_plate.save
       render json: {message: "UserPlate successfully created"}, status: 201
+    else
+      render json: { errors: user_plate.errors.full_messages }, status: 400
     end
   end
 end
--- spec/requests/api/v1/create_user_plates_request_spec.rb
+++ spec/requests/api/v1/create_user_plates_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "User Plates" do
-  describe "Create" do
+RSpec.describe "User Plates Create" do
+  describe "Happy Path" do
     before do
       test_data_posts
     end
@@ -23,6 +23,33 @@ RSpec.describe "User Plates" do
       @user_1.reload
       expect(@user_1.plates).to eq([@plate_1])
       expect(json[:message]).to eq("UserPlate successfully created")
+    end
+  end
+
+  describe "Sad Path" do
+    before do
+      test_data_posts
+    end
+
+    it "nonexistent user" do
+      post "/api/v1/user_plates", params: {
+        user_id: 98465765768,
+        plate_id: @plate_1.id
+      }
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(400)
+      expect(json[:errors].first).to eq("User must exist")
+    end
+
+    it "nonexistent plate" do
+      post "/api/v1/user_plates", params: {
+        user_id: @user_1.id,
+        plate_id: 32168498465
+      }
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(400)
+      expect(json[:errors].first).to eq("Plate must exist")
     end
   end
 end

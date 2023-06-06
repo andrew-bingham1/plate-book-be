e7df59bba98db235cdc81af5d05c0cd9f54dbd10
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-06-06 17:12:56 -0600

    top 5 hot plates

--- spec/requests/api/v1/hot_plates_spec.rb
+++ spec/requests/api/v1/hot_plates_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe "Hot Plates" do
     test_data_hot
   end
   it "returns top 5 plates by activity" do
-    get "/api/v1/search/plates"
-    require 'pry'; binding.pry
+    get "/api/v1/search/hot_plates"
+    plates = JSON.parse(response.body, symbolize_names: true)
+
+    expect(plates).to be_a(Hash)
+    expect(plates[:data].count).to eq(5)
+    expect(plates[:data]).to all(have_key(:type))
+    plates[:data].each do |plate|
+      expect(plate[:type]).to eq("plate")
+    end
   end
 end

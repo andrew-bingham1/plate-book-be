966b333673a3cb887a28c6f26841826bcb09ffb3
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-06-08 12:56:56 -0600

    comment editing sad path

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -9,7 +9,17 @@ class Api::V1::CommentsController < ApplicationController
   end
 
   def update
-    Comment.find(params[:comment_id]).update(body: params[:body])
+    if params[:comment_id]
+      comment = Comment.find(params[:comment_id])
+      if comment.update(body: params[:body])
+        render json: {}, status: 204
+      else
+        render json: {errors: "Failed to update comment"}, status: 400
+      end
+    else
+      render json: {errors: "Missing comment id"}, status: 404
+    end
+
   end
 
   private
--- spec/requests/api/v1/comments_update_request_spec.rb
+++ spec/requests/api/v1/comments_update_request_spec.rb
@@ -7,7 +7,16 @@ RSpec.describe "edit comment" do
   it "edits comment" do
     expect(@comment_6.body).to eq("Comment 6")
     put "/api/v1/posts/#{@post_18.id}/comments/#{@comment_6.id}", params: {comment_id: @comment_6.id, body: "I have a million Ducks"}
+    expect(response.body).to eq("")
+    expect(response).to have_http_status(204)
     @comment_6.reload
     expect(@comment_6.body).to eq("I have a million Ducks")
+  end
+
+  it "nonexistent body" do
+    put "/api/v1/posts/#{@post_18.id}/comments/#{@comment_6.id}", params: {comment_id: @comment_6.id, body: ""}
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to have_http_status(400)
+    expect(json[:errors]).to eq("Failed to update comment")
   end
 end
